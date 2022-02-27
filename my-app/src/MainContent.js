25a785d66a3a016d44cd95c62f755433300f309b
Sushant Kumar <sushantkumardbg@gmail.com>
2022-02-27 12:16:04 +0530

    initial main page

---
+++ my-app/src/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default function MainConten(){
+  return(
+    <>
+    <h1 className="body--title"> The content of the form should go below </h1>
+
+    </>
+  )
+}
